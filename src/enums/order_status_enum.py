b20302ea9673d4d76928b8a3d0f5f17d4c0be4fc
DialloAbdourahman <dialliabdourahman78@gmail.com>
2025-06-09 19:36:04 +0100

    corrections

--- src/enums/order_status_enum.py
+++ src/enums/order_status_enum.py
@@ -8,5 +8,7 @@ class EnumOrderStatus(Enum):
     PAYMENT_ERROR='PAYMENT_ERROR' # The user tried to pay but encountered an error, here we won't free the products and will allow the cron job to do it's work. The user can still come back to pay for the order or cancel it. On the front-end we can tell the user that he has one day to pay for the product. We will just change the status of the order to payment error. 
     CANCELLED_AUTOMATICALLY='CANCELLED_AUTOMATICALLY' # The cron job cancelled the order automatically. We free the products. 
 
-# Allow the user to retry failed payments by simply generating a new payment url. 
-# Maybe work on an admin route to retry a failed refund if a client complained. 
+# Maybe work on an admin route to retry a failed refund if a client complains. 
+# Work on uploading pictures of products while creating them. 
+# Work on rating products of a seller as a client. 
+# Containerize the app. 
